8bfc02b70e228a6b3c3ba7b6a8bf4a4d874d04ca
Pi-Machine <pipitwopi@gmail.com>
2016-08-05 13:11:22 -0400

    basics

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+//console.log('This would be the main JS file.');
+var myhtml = document.querySelector('html');
+var about = document.querySelector('h1');
+about.onclick = function() {
+	alert('HI');
+}
+
